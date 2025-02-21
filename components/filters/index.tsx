7f0df684a1a761dd301d5ef1d19a53c80ce6fbaf
shehab-fekry <shehabeddin99@gmail.com>
2025-02-22 00:37:18 +0200

    Enhance Filters component styling for better responsiveness and visual clarity

--- components/filters/index.tsx
+++ components/filters/index.tsx
@@ -18,7 +18,7 @@ const FILTERS = [
 const Filters = () => {
     const router = useRouter();
     const searchParams = useSearchParams();
-    const [active, setActive] = useState<string>("");
+    const [active, setActive] = useState<string>(searchParams.get("filter") || "");
     
     const onClickFilter = (filter: string) => {
         const utrlObj = decodeUrlToObject(searchParams.toString());
@@ -36,10 +36,14 @@ const Filters = () => {
     }
 
     return (
-        <section className='flex gap-3'>
+        <section className='hidden sm:flex sm:gap-3 flex-wrap'>
             {FILTERS.map(filter => {
                 return <Button
-                className={cn({"!bg-red-400 !text-red-950": active === filter.value})} 
+                className={cn("body-medium rounded-lg px-6 py-3 capitalize shadow-none",
+                   active === filter.value 
+                   ? "bg-primary-100 text-primary-500 hover:bg-primary-100 dark:bg-dark-400 dark:text-primary-500 dark:hover:bg-dark-400" 
+                   : "bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300"
+                )} 
                 key={filter.name} 
                 onClick={() => onClickFilter(filter.value)}>{filter.name}</Button>
             })}
